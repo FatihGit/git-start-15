80911e64cf558a039b9c87d536af3f94576ee2c4
Fatih Eraslan <era14331@spengergasse.at>
2014-09-15 11:00:18 +0200

    Korrektur

--- gitstart/src/at/spenger/git/start/Haupt.java
+++ gitstart/src/at/spenger/git/start/Haupt.java
@@ -3,8 +3,8 @@ package at.spenger.git.start;
 public class Haupt {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Weld");
-		System.out.println("Hello Worlt");
+		System.out.println("Hallo Welt");
+		System.out.println("Hello World");
 
 	}
 
